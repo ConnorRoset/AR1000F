1cbe7fdf5b35af8e42bf8470682337e081a13ecd
ConnorRoset <croset.estem@gmail.com>
2016-12-28 15:46:46 -0600

    Closing on Dec 28, 2016

--- src/main/AR1000F.java
+++ src/main/AR1000F.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.io.File;
+
 /**
  * Created by connorroset on 12/28/16.
  */
@@ -8,8 +10,10 @@ public class AR1000F {
     AES myAES = new AES();
     NTier myNTier = new NTier();
     Paillier myPaillier = new Paillier();
+
     public static void main(String[] args) {
         System.out.println("Here we go!");
         System.out.println("blap");
+
     }
 }
